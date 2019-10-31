6d2f17fdaa898450933bb2fb4f726ff0f1cb848a
Joe Previte <jjprevite@gmail.com>
2019-10-31 16:47:46 -0700

    wip: add test with oclif

--- src/__tests__/generate.test.ts
+++ src/__tests__/generate.test.ts
@@ -1,5 +1,7 @@
 import * as fse from "fs-extra";
 import * as path from "path";
+import { test } from "@oclif/test";
+import { execSync } from "child_process";
 import { makeTempDir, checkDirExists, removeTempDir } from "../utils";
 
 let root = process.cwd();
@@ -15,6 +17,11 @@ describe("The `generate` command", () => {
     // create our temporary directory
     try {
       await makeTempDir(tempRoot);
+      await execSync("yarn init -y", { cwd: tempRoot });
+      await makeTempDir(tempRoot + "/src");
+      await makeTempDir(tempRoot + "/src/components");
+      // TODO replace with eb-scirpts init command
+      // install eb-scripts as a dev dependency
     } catch (error) {
       console.error(error);
     }
@@ -29,9 +36,28 @@ describe("The `generate` command", () => {
     }
   });
 
-  it("expects to find a temporary directory", async () => {
+  it("expects to find a temporary directory with a package.json", async () => {
     const tempDirectoryExists = await checkDirExists(tempRoot);
+    const hasPackageJson = await fse.existsSync(`${tempRoot}/package.json`);
 
     expect(tempDirectoryExists).toBe(true);
+    expect(hasPackageJson).toBe(true);
+  });
+
+  it("runs using @oclif/test", async () => {
+    // STOPPED HERE
+    // Need to log all the files in the tempRoot
+    // then log after running test and see if it is working as expected
+    test
+      .loadConfig({
+        root: tempRoot
+      })
+      .stdout()
+      .command(["generate", "-t react-component", "-n HeyJoe"]);
+
+    const newComponentFolderExists = await checkDirExists(
+      tempRoot + "/src/components/HeyJoe"
+    );
+    expect(newComponentFolderExists).toBe(true);
   });
 });
