867c97a5de13672922c10918f4a378899340952c
Joe Previte <jjprevite@gmail.com>
2019-10-31 21:22:39 -0700

    chore: add checkFileExists to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { removeTempDir } from "./removeTempDir";
 export { checkDirExists } from "./checkDirExists";
 export { makeTempDir } from "./makeTempDir";
+export { checkFileExists } from "./checkFileExists";
